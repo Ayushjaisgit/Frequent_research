b05a54696cd2c474953955ccb639617fcd3353d7
Ayushjaisgit <ayushcodez@gmail.com>
2023-07-20 13:36:17 +0530

    useless code removed

--- db.js
+++ db.js
@@ -1,26 +1,4 @@
-// const mongoose = require('mongoose');
-// mongoose.set("strictQuery", false);
-
-
 const mongoURI = "mongodb://localhost:27017/formData?tls=false&readPreference=primary&directConnection=true"
-
-
-// const connectToMongo = ()=>{
-//     mongoose.connect(mongoURI, (err, res)=>{
-//         if(err){
-//             setTimeout( () => connectToMongo() , 3000)
-//         }
-//         console.log("connected to mongo successfully");
-//     })
-// }
-
-
-// connectToMongo();
-
-// module.exports = connectToMongo;
-
-
-// import MongoStore from 'connect-mongo';
 
 const mongoose = require('mongoose')
 
